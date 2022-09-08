c2df3f9c7933e8b0748c0e348af22f0a299c5df5
Celso Bastos <celsorico@gmail.com>
2022-09-08 20:29:49 -0300

    JS

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,2 +1,8 @@
-document.querySelector('.header').style.backgroundColor = '#0000ff';
-alert('olá mundo');
+var objeto = document.querySelector('.header');
+objeto.addEventListener('click', () => {
+    objeto.style.backgroundColor = '#0000ff';    
+});
+objeto.addEventListener('mouseout', () => {
+    objeto.style.backgroundColor = '#000';    
+});
+//console.dir(objeto);
