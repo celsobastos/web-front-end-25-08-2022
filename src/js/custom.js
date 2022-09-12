4980a61fba231bb347570657198264f480527eec
Celso Bastos <celsorico@gmail.com>
2022-09-12 19:40:59 -0300

    funções

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,6 +1,9 @@
 "use strict"
 
-let settings = JSON.parse(localStorage.getItem('pageSettings'));
-$('header').style.backgroundColor = settings.cor;
-$('barra-contato').style.fontSize = settings.fontSize;
+function aplicaConfiguracoes() {
+    let settings = JSON.parse(localStorage.getItem('pageSettings'));
+    $('header').style.backgroundColor = settings.cor;
+    $('barra-contato').style.fontSize = settings.fontSize;
+}
 
+setInterval(aplicaConfiguracoes, 1000);
