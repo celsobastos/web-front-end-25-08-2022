fe7ba087ed1fefdf182b45b7bed2529007975bab
Celso Bastos <celsorico@gmail.com>
2022-09-13 22:36:29 -0300

    prototype

---
+++ src/js/usandoJquery.js
@@ -0,0 +1,31 @@
+$(function() {
+
+    let config = {
+        color: '#fff',
+        backgroundColor: '#0000ff',
+        fontSize: '1rem',
+    };
+    let configOut = {
+        color: '#fff',
+        backgroundColor: '#000',
+        fontSize: '1rem',
+    };
+    // $('div').hover(function () {
+    //     $('div:even').css(config);
+    // });
+
+    $('div').on({
+        mouseover : function () {
+            $('div').css(configOut);
+        },
+        mouseout : function () {
+            $('div').css(config);
+        },
+        click : function () {
+            $('div').css('color', '#ffff00');
+        },
+    });
+
+
+
+});
